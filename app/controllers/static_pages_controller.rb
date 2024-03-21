710b798773eba4f60934d1055af96df635bf2bb1
Guy McDonald <guy@goodfishgroup.com>
2024-03-21 16:19:45 +0000

    Added routing and pages and controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+    render html: "Fuck you"
   end
 
   def about
+    render html: "About page"
   end
 
   def projects
+    render html: "Projects page"
   end
 
   def contact
+    render html: "Contact page"
   end
 end
