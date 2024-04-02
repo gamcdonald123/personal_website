722af0ef23685a7cfd2f28d306a6ca5a6e634df2
gamcdonald123 <gamcdonald@icloud.com>
2024-04-02 15:10:35 +0100

    Update static pages and improve navigation layout
    This commit updates the display content and routing for the static pages in the application as follows: 'home', 'about', 'projects', 'contact', and 'resume'. The home page now shows the current time. The underlying HTML for the static pages was removed, and the correct links for navigation have been applied. Improved CSS styling has also been added to application.css to better present the content layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,2 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "Hello world!"
-  end
-
-  def contact
-    render html: "Contact page"
-  end
-
-  def projects
-    render html: "Project"
-  end
 end
